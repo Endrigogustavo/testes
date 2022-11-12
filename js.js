84e30af0a3a4538095b43f4d0a0548c57f623656
Endrigogustavo <endrigogustavo2006@gmail.com>
2022-11-11 21:41:59 -0300

    commit

---
+++ js.js
@@ -0,0 +1,9 @@
+function img01 (){
+    document.getElementById("trocarimg").src="R (1).jpg";
+}
+function img02 (){
+    document.getElementById("trocarimg").src="R.jpg";
+}
+function img03 (){
+    document.getElementById("trocarimg").src="jk.png";
+}
